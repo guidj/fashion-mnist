f5052ddbcf303d80cfe53cd57c9fb81449569463
guilherme <guilherme.dinisjunior@gmail.com>
2019-12-22 02:18:38 +0100

    Adds hps bash script

---
+++ sbin/task-local-hps.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -xe
+
+function run(){
+
+    DIR=$(dirname $0)
+    BASE=${DIR}/..
+    SBIN=${BASE}/sbin
+    source ${SBIN}/env.sh
+    cd ${BASE}
+
+    bash ${SBIN}/package.sh
+
+    export PYTHONPATH=$PYTHONPATH:${BASE}/py/dist/fmnist-0.1.0-py3.7.egg
+
+    now=`date +%s`
+    job_id="fme_${now}-XXXXXXX"
+    TMP_DIR=$(mktemp -d -t $job_id)
+    python -m fmnist.hps.tune \
+        --train-data "${HOME}/code/fashion-mnist/data" \
+        --job-dir "${TMP_DIR}/job_dir" \
+        --model-dir "${TMP_DIR}/model_dir" \
+        --num-threads 4 \
+        --num-epochs 25 \
+        --max-evaluations 200 "$@"
+    cp -r "${TMP_DIR}" "${HOME}/"
+}
+
+run "$@"
